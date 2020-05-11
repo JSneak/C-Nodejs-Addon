8e24969708c0115403a3bbeb7c752b5299572106
Sean Kim <thesean.kim33@gmail.com>
2020-05-11 02:45:25 -0500

    Working Demo from video
    https://www.youtube.com/watch?v=O4Rlq_NSkoM

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-C++ Node.js Add-On
+In order to build
+
+1) Install node-gyp
+1) node-gyp configure
+2) node-gyp build
+3) Your file should now be in /build/Release/[name of file]
---
+++ index.js
@@ -0,0 +1,9 @@
+const calculate = require('./build/Release/calculate');
+
+
+
+console.time('c++');
+calculate.calc();
+console.timeEnd('c++');
+
+// console.log(calculate.calc());
